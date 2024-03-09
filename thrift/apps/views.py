8df8e1d03f37dc415991f3280828bb547aa0ed77
charitraa <stharabi9862187405@gmail.com>
2024-03-10 00:04:15 +0545

    new

--- thrift/apps/views.py
+++ thrift/apps/views.py
@@ -3,7 +3,6 @@ from .models import User
 # Create your views here.
 
 def signup(request):
-    user = User.objects.all()
 
     if request.method =='POST':
         uname = request.POST['username']
@@ -13,7 +12,6 @@ def signup(request):
         repassword = request.POST['repassword']
         phone = request.POST['phone']
         gender = request.POST['gender']
-        
         user = User.objects.filter(Email = email)
 
         if user:
@@ -24,5 +22,5 @@ def signup(request):
                 if newuser:
                     return redirect('signup')
                 
-    return render(request,"Signup.html",{'prams':user})
+    return render(request,"Signup.html")
 
